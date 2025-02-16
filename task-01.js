36c5be6d598757922b518bdf4275c76965242253
Md. Habibur Rahman Bappy <habib.rahman@live.com>
2025-02-16 17:20:13 +0600

    task-01

---
+++ task-01.js
@@ -0,0 +1,10 @@
+// Take four parameters. Multiply the four numbers and then return the result
+
+function multiply(num1, num2, num3, num4) {
+  let result = num1 * num2 * num3 * num4;
+  return result;
+}
+
+const functionResult = multiply(1, 2, 3, 4);
+
+console.log(functionResult);
